9b9054e7616dc89c87884f57423b1fd278989142
EzzEl-DeenMohamed <Ezzasdezz123@gmail.com>
2024-04-23 08:23:04 +0200

    ~

--- is322_HW_1/src/invertedIndex/Test.java
+++ is322_HW_1/src/invertedIndex/Test.java
@@ -7,7 +7,7 @@ package invertedIndex;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * 
@@ -44,8 +44,10 @@ public class Test {
 
         do {
             System.out.println("Print search phrase: ");
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            phrase = in.readLine();
+//            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            Scanner scanner = new Scanner(System.in);
+            phrase = scanner.nextLine();
+
             if (!phrase.isEmpty()) {
                 // Perform the search and display the results
                 System.out.println("Search results for \"" + phrase + "\":\n" + index.find_24_01(phrase));
